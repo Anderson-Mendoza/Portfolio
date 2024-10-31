fdeaf18b31656734bb789987abee1d97aae6664b
Anderson Mendoza <mendozaanderson1195@gmail.com>
2024-10-31 16:39:47 -0500

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Hero from "./components/Hero";
 import Tech from "./components/technologies";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 
 function App() {
   return (
     <main>
       <Hero></Hero>
+      <Projects></Projects>
       <About></About>
       <Tech></Tech>
     </main>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <div className="container">
+            <h2>Proyectos</h2>
+        </div>
+    )
+}
